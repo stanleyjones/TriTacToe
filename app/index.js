e3777dd3f654fb4aa17fca4798eb5eebf515060b
Stanley Jones <stanley@chartboost.com>
2016-09-21 09:22:43 -0700

    Merged advanced settings.

--- app/index.js
+++ app/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { applyMiddleware, createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './containers/App';
 import rootReducer from './reducers';
 
-const logger = store => next => action => {
-  console.log('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  return result;
-};
-
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer);
 
 export default function TriTacToe() {
   return <Provider store={store}><App /></Provider>;
